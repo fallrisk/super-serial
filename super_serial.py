84e45cfa7443269da24211f282c563134a8b3091
Justin Watson <jwatson5@gmail.com>
2017-11-30 09:45:28 -0800

    First commit.

---
+++ super_serial.py
@@ -0,0 +1,71 @@
+"""
+Copyright Justin Watson 2017
+
+"""
+
+import sys
+
+from PyQt5 import QtCore, QtWidgets
+
+# http://pyqt.sourceforge.net/Docs/PyQt5/gotchas.html#crashes-on-exit
+app = None
+
+class ApplicationWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
+        self.setWindowTitle("Super Serial")
+
+        self.file_menu = QtWidgets.QMenu('&File', self)
+        self.file_menu.addAction('&Quit', self.fileQuit,
+                                 QtCore.Qt.CTRL + QtCore.Qt.Key_Q)
+        self.menuBar().addMenu(self.file_menu)
+
+        self.help_menu = QtWidgets.QMenu('&Help', self)
+        self.menuBar().addSeparator()
+        self.menuBar().addMenu(self.help_menu)
+
+        self.help_menu.addAction('&About', self.about)
+
+        self.main_widget = QtWidgets.QWidget(self)
+
+        l = QtWidgets.QVBoxLayout(self.main_widget)
+
+        self.main_widget.setFocus()
+        self.setCentralWidget(self.main_widget)
+
+        self.statusBar().showMessage("All hail matplotlib!", 2000)
+
+    def fileQuit(self):
+        self.close()
+
+    def closeEvent(self, ce):
+        self.fileQuit()
+
+    def about(self):
+        QtWidgets.QMessageBox.about(self, "About",
+                                    """embedding_in_qt5.py example
+Copyright 2005 Florent Rougon, 2006 Darren Dale, 2015 Jens H Nielsen
+
+This program is a simple example of a Qt5 application embedding matplotlib
+canvases.
+
+It may be used and modified with no restriction; raw copies as well as
+modified versions may be distributed without limitation.
+
+This is modified from the embedding in qt4 example to show the difference
+between qt4 and qt5""")
+
+
+def main():
+	global app
+	app = QtWidgets.QApplication(sys.argv)
+
+	aw = ApplicationWindow()
+	aw.show()
+
+	app.exec()
+
+
+if __name__ == '__main__':
+	main()
