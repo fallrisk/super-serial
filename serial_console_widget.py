9aee3a5bb141e54e88eec88c6e790e79404b96ee
Justin Watson <jwatson5@gmail.com>
2017-12-14 11:39:54 -0800

    Basic syntax highlighting.

--- serial_console_widget.py
+++ serial_console_widget.py
@@ -31,6 +31,15 @@ class SerialConsoleWidget(QtWidgets.QTextEdit):
         self.show_crlf = False
         self.ccp = ControlCharPainter()
         self.ccp.setTextEdit(self)
+        #self.unicode_font = QtGui.QFont('Segoe UI Symbol', 12)
+
+        self.__highligter = Highlighter(self)
+        self.__highlight_timer = QtCore.QTimer(self)
+        self.__highlight_timer.timeout.connect(self.__highligter.highlight)
+        self.__highlight_timer.start(1000)
+
+        # TODO: Run highlight once every time new data is provided for the widget.
+
         #self.ruler = Ruler(self)
         #self.my_layout = SuperTextLayout(self.document())
         #self.document().setDocumentLayout(SuperTextLayout(self.document()))
@@ -49,28 +58,35 @@ class SerialConsoleWidget(QtWidgets.QTextEdit):
 
     def putData(self, data):
         # for c in data:
-        #     if not self.show_crlf:
-        #         self.insertPlainText(c)
-        #         continue
+        #     print(ord(c))
+        #     # if not self.show_crlf:
+        #     #     self.insertPlainText(c)
+        #     #     continue
         #     if c == '\n' or c == '\r':
-        #         ctrlCharFormat = QtGui.QTextCharFormat()
-        #         ctrlCharFormat.setObjectType(ControlCharFormat)
-        #         ctrlCharFormat.setProperty(1, ord(c))
-        #         orc = chr(0xfffc)
-        #         cursor = self.textCursor()
-        #         cursor.insertText(orc, ctrlCharFormat)
-        #         self.setTextCursor(cursor)
+        #         # ctrlCharFormat = QtGui.QTextCharFormat()
+        #         # ctrlCharFormat.setObjectType(ControlCharFormat)
+        #         # ctrlCharFormat.setProperty(1, ord(c))
+        #         # orc = chr(0xfffc)
+        #         # cursor = self.textCursor()
+        #         # cursor.insertText(orc, ctrlCharFormat)
+        #         # self.setTextCursor(cursor)
         #         self.insertPlainText(c)
         #     elif ord(c) < 0x20:
-        #         ctrlCharFormat = QtGui.QTextCharFormat()
-        #         ctrlCharFormat.setObjectType(ControlCharFormat)
-        #         ctrlCharFormat.setProperty(1, ord(c))
-        #         orc = chr(0xfffc)
+        #         #ctrlCharFormat = QtGui.QTextCharFormat()
+        #         #ctrlCharFormat.setObjectType(ControlCharFormat)
+        #         #ctrlCharFormat.setProperty(1, ord(c))
+        #         #orc = chr(0xfffc)
+        #         prev_font = self.font()
+        #         #self.setFont(self.unicode_font)
+        #         print('changing char')
         #         cursor = self.textCursor()
-        #         cursor.insertText(orc, ctrlCharFormat)
+        #         nc = chr(ord(c) + 2400)
+        #         self.insertPlainText(nc)
+        #         self.setFont(prev_font)
+        #         #cursor.insertText(orc, ctrlCharFormat)
         #         self.setTextCursor(cursor)
         #     else:
-        self.insertPlainText(c)
+        self.insertPlainText(data)
         vbar = self.verticalScrollBar()
         vbar.setValue(vbar.maximum())
 
@@ -91,8 +107,9 @@ class ControlCharObject(QtCore.QObject, QtGui.QTextObjectInterface):
 class ControlCharPainter(QtCore.QObject):
 
     def _onContentsChange(self, position, chars_removed, chars_added):
-        print('contents changed {} {} {}'.format(position, chars_removed, chars_added))
-        print(self._text_edit_widget.document().documentLayout())
+        #print('contents changed {} {} {}'.format(position, chars_removed, chars_added))
+        #print(self._text_edit_widget.document().documentLayout())
+        return
 
     def setTextEdit(self, text_edit_widget):
         self._text_edit_widget = text_edit_widget
@@ -140,3 +157,34 @@ class SuperRawFont(QtGui.QRawFont):
         print('here')
         return super(SuperRawFont, self).pathForGlyph(glyphIndex)
 
+
+class Highlighter(QtCore.QObject):
+    def __init__(self, parent):
+        super(Highlighter, self).__init__()
+        self.__parent = parent
+
+    def highlight(self):
+        """
+        This should be attached to a QTimer.
+        """
+        char_format = QtGui.QTextCharFormat()
+        cursor = self.__parent.textCursor()
+        # Format properties are in the following to classes.
+        # http://doc.qt.io/qt-5/qtextformat.html#public-functions
+        # http://doc.qt.io/qt-5/qtextcharformat.html
+        char_format.setForeground(QtGui.QBrush(QtGui.QColor('red')))
+        pattern = 'FW'
+        # http://doc.qt.io/qt-5/qregexp.html
+        regex = QtCore.QRegExp(pattern)
+        pos = 0
+        index = regex.indexIn(self.__parent.toPlainText(), pos)
+        while index != -1:
+            # Select the matched text and apply the format.
+            #print(index, index + regex.matchedLength())
+            cursor.setPosition(index)
+            cursor.setPosition(index + regex.matchedLength(), QtGui.QTextCursor.KeepAnchor)
+            # http://doc.qt.io/qt-5/richtext-cursor.html
+            cursor.mergeCharFormat(char_format)
+            # Move to the next match.
+            pos = index + regex.matchedLength()
+            index = regex.indexIn(self.__parent.toPlainText(), pos)
